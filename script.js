ca7e758e01d0efde29b193ebb9420b4271d53b19
nausin18 <nayeemanausin@gmail.com>
2024-10-30 00:47:15 +0530

    updated

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById("hamburger").addEventListener("click", function() {
+    document.querySelector(".navbar").classList.toggle("active"); // Toggle active class on navbar
+});
